6b940dabc053d1bd184d0179c7ac15529535fc23
Lee-412 <leeduchht@gmail.com>
2025-02-14 21:31:07 +0700

    fix(main, index): fix some bug with router

--- src/route/AppRouter.tsx
+++ src/route/AppRouter.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 
 
 const AppRouter = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
     );
 };
 
